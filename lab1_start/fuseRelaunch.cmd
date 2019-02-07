62f34f08d077f4fcf4156f1c221f444714f18449
Billy Koech <bkkoech@college.harvard.edu>
2019-02-07 14:57:51 -0500

    added gitignore

---
+++ lab1_start/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "Z:/Shared files/lab1_start/testbench_isim_beh.exe" -prj "Z:/Shared files/lab1_start/testbench_beh.prj" "work.testbench" "work.glbl" 
---
+++ lab1_start/main.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps  //sets the timescale (for simulation)
+`default_nettype none //overrides default behaviour: errors when a net type is not declired
+
+module main(switch, led);
+	// declare inputs and outputs
+	input wire [7:0] switch;
+	output wire [7:0] led;
+
+	assign led[7:4] = 4'b0000;
+	// Question 1
+	assign led[0] = switch[0] & switch[1];
+	//Question 2
+	assign led[1] = ~(switch[2] & ~switch[3]);
+	//Question 3
+	// 8 bit XNOR gate
+	assign led[2] = ~^switch;
+	//Question 4
+	// Mux
+	assign led[3] = switch[4] ? led[0] : led[1];
+
+endmodule
+
+`default_nettype none //reengages default behaviour: needed for other Xilinx modules
