49dfc0c0d9ccb01947c7b14312668eac9f060bb8
Nicolas Weninger <nicolas@Nicolass-MacBook-Pro.local>
2019-02-20 21:01:42 -0500

    changed ALU

--- Final/PA2 pt2/adder_32_bit.v
+++ Final/PA2 pt2/adder_32_bit.v
@@ -12,22 +12,24 @@
 module adder_32_bit(A, B, Cout, Z);
 
 	//parameter definitions
-	input wire [31:0] A, B;
-	output wire [31:0] Z;
+	parameter N = 32;
+	
+	input wire [N-1:0] A, B;
+	output wire [N-1:0] Z;
 	output wire Cout;
 	
-	wire carrys [32:0];
+	wire carrys [N:0];
 	
 	assign carrys[0] = 0;
 	
 	generate
 		genvar i;
-		for (i = 0; i < 32; i = i + 1) begin
+		for (i = 0; i < N; i = i + 1) begin
 			one_bit_full_adder adder_gen (.A(A[i]), .B(B[i]), .Cin(carrys[i]), .Cout(carrys[i+1]), .Z(Z[i]));
 		end
 	endgenerate
 	
-	assign Cout = carrys[32];
+	assign Cout = carrys[N];
 	
 
 endmodule
--- Final/PA2 pt2/one_bit_full_adder.v
+++ Final/PA2 pt2/one_bit_full_adder.v
@@ -10,7 +10,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module one_bit_full_adder(A, B, Cin, Cout, Z);
-
+	
 	//port definitions - customize for different bit widths
 	input wire A, B, Cin; //two 1 bit inputs and Carry in
 	output wire Z, Cout; // one 1 bit carry out
