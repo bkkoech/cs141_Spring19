f2cd78ee9b6979500cdcae1d499b86e9af42dffa
Billy Koech <bkkoech@college.harvard.edu>
2019-04-03 22:18:48 -0400

    added ADD to mips_funct_defines

---
+++ Billy/lab4_start_part 2/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "Z:/Shared files/Billy/lab4_start_part 2/test_mips_multicycle_isim_beh.exe" -prj "Z:/Shared files/Billy/lab4_start_part 2/test_mips_multicycle_beh.prj" "work.test_mips_multicycle" "work.glbl" 
--- Final/PA4 pt1/mips_funct_defines.v
+++ Final/PA4 pt1/mips_funct_defines.v
@@ -1,6 +1,6 @@
 `ifndef MIPS_FUNCT_DEFINES
 `define MIPS_FUNCT_DEFINES
-
+`define FUNCT_ADD d'32
 `define 
 
 `endif
