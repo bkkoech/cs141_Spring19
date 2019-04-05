d7cc86b930da6fe78da19d118edaac6ca365acef
Nicolas Weninger <nicolas@Nicolass-MacBook-Pro.local>
2019-04-05 16:05:43 -0400

    added definition files

--- Final/PA4 pt1/mips_op_codes_defines.v
+++ Final/PA4 pt1/mips_op_codes_defines.v
@@ -1,6 +1,17 @@
 `ifndef MIPS_OP_CODES_DEFINES
 `define MIPS_OP_CODES_DEFINES
 
-`define 
+`define RTYPE 5'd0
+`define J 5'd2
+`define JAL 5'd3
+`define BEQ 5'd4
+`define BNE 5'd5
+`define ADDI 5'd8
+`define SLTI 5'd10
+`define ANDI 5'd12
+`define ORI 5'd13
+`define XORI 5'd14
+`define LW 5'd35
+`define SW 5'd43
 
 `endif
