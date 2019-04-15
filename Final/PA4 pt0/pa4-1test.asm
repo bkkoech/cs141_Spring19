430ff366b2114b7ea5ada3adfd4a91faef5d37a0
Nicolas Weninger <nicolas@Nicolass-MacBook-Pro.local>
2019-04-14 22:53:57 -0400

    asm update

--- Final/PA4 pt0/pa4-1test.asm
+++ Final/PA4 pt0/pa4-1test.asm
@@ -1,10 +1,9 @@
-sw 	$t0, 16($0)
-nor $t1, $t1, $t1
-sw $t1 4($0)
-lw $t2, 4($0)
-
-nor $t0, $t0, $t0
+nor $t0, $t0, $t0		# load 1s into t0
 addi    $t1, $t0, -15   # 4 LSBs=0
 ori     $t2, $t1, 7     # one zero at 4th pos
 xori    $t3, $t2, 8     # all 1s
 slti    $t4, $0, 1      # lsb = 1
+sw 	$t0, 16($0)			# DMEM 0x4 has all 1s
+sw $t1 4($0)			# DMEM 0x1 has all 1s
+lw $t5, 4($0)			# t5 has all 1s
+
