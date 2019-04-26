912d9dead81d81e63d78a36e6a0e4dccf7e05063
Nicolas Weninger <nicolas@Nicolass-MacBook-Pro.local>
2019-04-26 13:55:13 -0400

    sac initial

--- Nick/PA5/set_associative.c
+++ Nick/PA5/set_associative.c
@@ -62,14 +62,110 @@ static int lru(set_associative_cache* sac, int set)
 
 void sac_store_word(set_associative_cache* sac, void* addr, unsigned int val)
 {
-    // TODO
+    memory_block* cacheline = NULL;
+    int set = 0;
+    int way = 0;
+
+    // Precompute start address of block
+    size_t addr_offt = (size_t) (addr - MAIN_MEMORY_START_ADDR) % MAIN_MEMORY_BLOCK_SIZE;
+    void* mb_start_addr = addr - addr_offt;
+
+    // compute set from addr
+    set = addr_to_set(mb_start_addr);
+
+    // look through set ways to see if already present
+    for (int j = 0; j < SET_ASSOCIATIVE_NUM_WAYS; j++){
+        if (sac->cache[set][j] != NULL && sac->cache[set][j]->start_addr == mb_start_addr){
+            cacheline = sac->cache[set][j];
+            way = j;
+        }
+    }
+
+    // if hit
+    if (cacheline != NULL){
+        unsigned int* mb_addr = cacheline->data + addr_offt;
+        *mb_addr = val;
+        sac->dirty_bits[set][way] = true;
+        mark_as_used(sac, set, way);
+    }
+    //if miss, need to evict lru
+    else {
+        ++sac->cs.w_misses;
+        way = lru(sac, set);
+        cacheline = sac->cache[set][way];
+        // check dirty bit
+        if (cacheline != NULL && sac->dirty_bits[set][way] == true){
+            mm_write(sac->mm, cacheline->start_addr, cacheline);
+        }
+        if(cacheline != NULL){
+            mb_free(cacheline);
+        }
+        cacheline = mm_read(sac->mm, mb_start_addr);
+        sac->cache[set][way] = cacheline;
+        unsigned int* mb_addr = cacheline->data + addr_offt;
+        *mb_addr = val;
+        sac->dirty_bits[set][way] = true;
+        mark_as_used(sac, set, way);
+    }
+    // Update statistics
+    ++sac->cs.w_queries;
 }
 
 
 unsigned int sac_load_word(set_associative_cache* sac, void* addr)
 {
-    // TODO
-    return 0;
+    memory_block* cacheline = NULL;
+    unsigned int* mb_addr = 0;
+    int set = 0;
+    int way = 0;
+    int result = 0;
+
+    // Precompute start address of block
+    size_t addr_offt = (size_t) (addr - MAIN_MEMORY_START_ADDR) % MAIN_MEMORY_BLOCK_SIZE;
+    void* mb_start_addr = addr - addr_offt;
+
+    // compute set from addr
+    set = addr_to_set(mb_start_addr);
+
+    // look through set ways to see if already present
+    for (int j = 0; j < SET_ASSOCIATIVE_NUM_WAYS; j++){
+        if (sac->cache[set][j] != NULL && sac->cache[set][j]->start_addr == mb_start_addr){
+            cacheline = sac->cache[set][j];
+            way = j;
+        }
+    }
+
+    //if hit
+    if (cacheline != NULL){
+        mb_addr = cacheline->data + addr_offt;
+        result = *mb_addr;
+        mark_as_used(sac, set, way);
+    }
+    // if miss, evict
+    else {
+        ++sac->cs.r_misses;
+        way = lru(sac, set);
+        cacheline = sac->cache[set][way];
+        
+        //check dirty bit
+        if (cacheline != NULL && sac->dirty_bits[set][way] == true){
+            mm_write(sac->mm, cacheline->start_addr, cacheline);
+        }
+        if (cacheline != NULL){
+            mb_free(cacheline);
+        }
+        // overwrite by reading data
+        cacheline = mm_read(sac->mm, mb_start_addr);
+        sac->dirty_bits[set][way] = false;
+        sac->cache[set][way] = cacheline;
+        mb_addr = cacheline->data + addr_offt;
+        result = *mb_addr;
+        mark_as_used(sac, set, way);
+    }
+
+    // Update statistics
+    ++sac->cs.r_queries;
+    return result;
 }
 
 void sac_free(set_associative_cache* sac)
