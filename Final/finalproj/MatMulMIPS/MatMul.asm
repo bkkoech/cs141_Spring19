739b3d896cf9a5781b543fa3dbde049e9f6d11fb
Nicolas Weninger <nicolas@Nicolass-MacBook-Pro.local>
2019-05-04 20:46:51 -0400

    init comments in asm

---
+++ Final/finalproj/MatMulMIPS/MatMul.asm
@@ -0,0 +1,15 @@
+# --------------------REGISTER ASSIGNMENTS--------------------
+
+# $s0 matrix result value
+# $s1 dim matrix dimention
+
+# $t0 res result matrix read address
+# $t1 loc1 matrix 1 read address
+# $t2 loc2 matrix 2 read address
+# $t3 count_row counter 1-8 for result matrix row position
+# $t4 count_col counter 1-8 for result matrix column position
+# $t5 dim_count counter 1-8 for calculations loop
+
+# $t6 temporary for loc2 calcs
+
+# ------------------------------------------------------------
